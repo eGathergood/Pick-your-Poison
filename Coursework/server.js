389d95b9187c3b0dcbc82219c912766375132ddd
OGOCHUKWU EMELE (1405932) <1405932@rgu.ac.uk>
2018-03-26 16:40:16 +0100

    server

--- Coursework/server.js
+++ Coursework/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
- res.send("Hello world! by express");
+ res.sendFile('index.html');
 });
 app.listen(8080);

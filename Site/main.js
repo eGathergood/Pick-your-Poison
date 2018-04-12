f48a366f7bc7b51c220dbd178f4b1f0826daf62c
eGathergood <ewan_good@hotmail.co.uk>
2018-04-12 17:59:08 +0100

    Example APi call working. input stored

--- Site/main.js
+++ Site/main.js
@@ -1,6 +1,8 @@
 $('#drinkSearch').click(function(){
- event.preventDefault();
+  var word = document.getElementById("sbar").value;
+  event.preventDefault();
   console.log("All these contain gin");
+  console.log(word)
 
     $.getJSON("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita", function(Result) {
     	console.log(Result)

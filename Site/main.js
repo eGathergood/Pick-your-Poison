1bec2c552ff96162fac7be0cd2313185a6d8faae
eGathergood <ewan_good@hotmail.co.uk>
2018-04-12 18:01:39 +0100

    Search results working

--- Site/main.js
+++ Site/main.js
@@ -1,10 +1,9 @@
 $('#drinkSearch').click(function(){
   var word = document.getElementById("sbar").value;
   event.preventDefault();
-  console.log("All these contain gin");
   console.log(word)
 
-    $.getJSON("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita", function(Result) {
+    $.getJSON("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+ word, function(Result) {
     	console.log(Result)
     });
 

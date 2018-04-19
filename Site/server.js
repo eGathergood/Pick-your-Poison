e4ac300bbc25c715063f31da315671feb762616e
GMitchell98 <g.mitchell9@rgu.ac.uk>
2018-04-19 14:09:05 +0100

    Update server.js

--- Site/server.js
+++ Site/server.js
@@ -5,6 +5,21 @@ app.get('/', function(req, res){
 });
 app.listen(8080);
 
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+  db.collection('users').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/login');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    //otherwise send them back to login
+    else{res.redirect('/login')}
+  });
+});
 
 
 app.post('/login', function(req, res) {

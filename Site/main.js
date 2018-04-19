b9ad9d2132c8cb93691d808654e2e9af264e73fd
eGathergood <ewan_good@hotmail.co.uk>
2018-04-19 17:12:47 +0100

    Moved inline js

--- Site/main.js
+++ Site/main.js
@@ -1,7 +1,34 @@
 //Search results hidden on page load
+//Add ingredient box on load
 $(document).ready(function(){
+    addIngredientBox();
     $("#resultsbox").toggle(false);
 });
+
+
+//Add additional ingredient box on click
+$('#Addings').on('click','.add-box',function(){
+  $('.form-wrapper').removeClass("add").addClass("remove");
+  $('.add-box').removeClass("add-box").addClass("remove-box").text("-");
+  addIngredientBox();
+
+});
+
+//Remove selected ingredient boxes on click
+$('#Addings').on('click','.remove-box',function(){
+  $(this).parents('form').remove();
+});
+
+
+//To add ingredient boxes
+function addIngredientBox(){
+  var formWrapper= $('<form class="form-wrapper cf add" onsubmit="return false"></form>');
+  var textBox=$('<input class="text-box" type="text"placeholder="List ingredients here..">');
+  var button=$('<button class="add-box">+</button>');
+  $(formWrapper).append(textBox);
+  $(formWrapper).append(button);
+  $(formWrapper).appendTo($('#wrapper'));
+}
 
 
 //Code executed when button is clicked

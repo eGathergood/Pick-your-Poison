be465974a72b22d3fe5c52446347b34bbf89edca
GMitchell98 <g.mitchell9@rgu.ac.uk>
2018-04-19 13:50:11 +0100

    Added
    I added the sever route for creating a brand new user

--- Site/server.js
+++ Site/server.js
@@ -4,3 +4,27 @@ app.get('/', function(req, res){
  res.sendFile('/home/codio/workspace/Pick-your-Poison/Site/index.html');
 });
 app.listen(8080);
+
+
+
+app.post('/login', function(req, res) {
+  //check we are logged in
+  if(!req.session.loggedin){res.redirect('/login');return;}
+
+  //we create the data string from the form components that have been passed in
+
+var datatostore = 
+"name":req.body.username,
+ "email":req.body.email,
+  "password":req.body.password}
+ 
+
+
+//once created we just run the data string against the database and all our new data will be saved/
+  db.collection('users').save(datatostore, function(err, result) {
+    if (err) throw err;
+    console.log('saved to database')
+    //when complete redirect to the index
+    res.redirect('/')
+  })
+});

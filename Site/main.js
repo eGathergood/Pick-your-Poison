0510f1bb69f55cda0cfa40eb20f32a0f72524ddd
ogochukwu2610 <gogisbaby@gmail.com>
2018-04-19 18:03:21 +0100

    merging

--- Site/main.js
+++ Site/main.js
@@ -1,21 +1,84 @@
+//Search results hidden on page load
+//Add ingredient box on load
 $(document).ready(function(){
+    addIngredientBox();
     $("#resultsbox").toggle(false);
+
+    //to return box contents
+    function getSearchBox(){
+      var userInput="";
+      $(".form-wrapper input").each(function(){
+        userInput+=$(this).val()+',';
+      });
+      return userInput;
+    }
+
+    //run the ingredient search
+    $("#schbutton").click(function(){
+      console.log("clicked");
+      $('.results').remove();
+      searchApi(getSearchBox());
+    });
+    // clear all additional ingredient boxes available
+    $("#clrbutton").click(function(){
+      $('#wrapper').empty();
+      addIngredientBox();
+    });
 });
 
 
+
+
+
+
+
+//Add additional ingredient box on click
+$('#Addings').on('click','.add-box',function(){
+  $('.form-wrapper').removeClass("add").addClass("remove");
+  $('.add-box').removeClass("add-box").addClass("remove-box").text("-");
+  addIngredientBox();
+
+});
+
+//Remove selected ingredient boxes on click
+$('#Addings').on('click','.remove-box',function(){
+  $(this).parents('form').remove();
+});
+
+
+//To add ingredient boxes
+function addIngredientBox(){
+  var formWrapper= $('<form class="form-wrapper cf add" onsubmit="return false"></form>');
+  var textBox=$('<input class="text-box" type="text"placeholder="List ingredients here..">');
+  var button=$('<button class="add-box">+</button>');
+  $(formWrapper).append(textBox);
+  $(formWrapper).append(button);
+  $(formWrapper).appendTo($('#wrapper'));
+}
+
+
+//Code executed when button is clicked
   $('#drinkSearch').click(function(){
+    //Gather searchterms
     var word = document.getElementById("sbar").value;
+    //Show results div, hide default main page
     $( "#mainPage").toggle(false);
     $( "#resultsbox").toggle(true);
 
+    //Stop button from refreshing page (default function)
     event.preventDefault();
     console.log(word)
 
-
+    //Get API response using searchterms
       $.getJSON("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+ word, function(Result) {
       	console.log(Result)
 
+
+
+
         Result.drinks.forEach((drink) => {
+          var drinkName = drink.strDrink;
+          console.log(drinkName);
     const drinkEntries = Object.entries(drink),
       //Build arrays out of the two sets of keys
       [
@@ -52,7 +115,7 @@ $(document).ready(function(){
     console.log("Ingredients:", finalIngredients);
     console.log("Measures:", finalMeasures);
 
-  $('#results').append("<li>" + ingredientsWithMeasures + "</li>");
+    $('#resultsTable > tbody:last').append( "<tr><td>"+ drinkName + "</td> + <td>"+ ingredientsWithMeasures + "</td></tr>" );
 
 
     console.log("All ingredients and measures:\n", ingredientsWithMeasures
--- Site/server.js
+++ Site/server.js
@@ -1,6 +1,62 @@
-var express = require('express');
-var app = express();
+const MongoClient = require('mongodb').MongoClient; 
+const url = "mongodb://localhost:27017/users";
+const express = require('express');
+const app = express();
+const session = require('express-session'); 
+const bodyParser = require('body-parser'); 
+
+app.use(session({ secret: 'example' }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+var db;
+
+
 app.get('/', function(req, res){
- res.sendFile('~/workspace/Pick-your-Poison/Coursework/index.html');
+ res.send('/home/codio/workspace/Pick-your-Poison/Site/index.html');
 });
 app.listen(8080);
+console.log('listen on 8080');
+
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+  db.collection('users').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/login');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    //otherwise send them back to login
+    else{res.redirect('/login')}
+  });
+});
+
+
+app.post('/login', function(req, res) {
+  //check we are logged in
+  if(!req.session.loggedin){res.redirect('/login');return;}
+
+  //we create the data string from the form components that have been passed in
+
+var datatostore = 
+"name":req.body.username,
+ "email":req.body.email,
+  "password":req.body.password}
+ 
+
+
+//once created we just run the data string against the database and all our new data will be saved/
+  db.collection('users').save(datatostore, function(err, result) {
+    if (err) throw err;
+    console.log('saved to database')
+    //when complete redirect to the index
+    res.redirect('/')
+  })
+});

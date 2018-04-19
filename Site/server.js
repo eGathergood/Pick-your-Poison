95eefb5b24ad9c6d95c1fa06b4a765ac08c7e8d2
GMitchell98 <g.mitchell9@rgu.ac.uk>
2018-04-19 16:22:11 +0100

    Update server.js

--- Site/server.js
+++ Site/server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser'); 
 
 app.get('/', function(req, res){
- res.sendFile('/index.html');
+ res.render('/index.html');
 });
 app.listen(8080);
 console.log('listen on 8080');

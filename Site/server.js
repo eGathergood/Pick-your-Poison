a526b2610f6204ab22faa48566b64f0031445319
ogochukwu2610 <gogisbaby@gmail.com>
2018-04-23 01:11:04 +0100

    for about

--- Site/server.js
+++ Site/server.js
@@ -15,24 +15,44 @@ app.set('view engine', 'ejs');
 
 var db;
 
-
-app.get('/', function(req, res){
- res.render('/index.html');
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening on 8080');
 });
-app.listen(8080);
-console.log('listen on 8080');
+
+//root route
+app.get('/', function(req, res){
+<<<<<<< HEAD
+ res.render('/index.html');
+=======
+ res.redirect('/index'
+)});
+
+//login route
+app.all('/login', function(req, res) {
+  res.render('/login');
+});
+//about route
+app.all('/about', function(req, res) {
+  res.render('/about');
+>>>>>>> 7abf6bbb7235a7a410ee5fc686f27b90b55b7e45
+});
+
+
 
 app.post('/dologin', function(req, res) {
   console.log(JSON.stringify(req.body))
   var uname = req.body.username;
   var pword = req.body.password;
 
-  db.collection('users').findOne({"login.username":uname}, function(err, result) {
+  db.collection('people').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/profile') }
     //otherwise send them back to login
     else{res.redirect('/login')}
   });
@@ -41,19 +61,28 @@ app.post('/dologin', function(req, res) {
 
 app.post('/login', function(req, res) {
   //check we are logged in
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!req.session.loggedin){res.redirect('/profile');return;}
 
   //we create the data string from the form components that have been passed in
 
+<<<<<<< HEAD
 var datatostore =
 "name":req.body.username,
  "email":req.body.email,
   "password":req.body.password}
 
+=======
+var datatostore = {
+"name":req.body.username,
+ "email":req.body.email,
+  "password":req.body.password,
+    "drink":req.body.drink}
+ 
+>>>>>>> 7abf6bbb7235a7a410ee5fc686f27b90b55b7e45
 
 
 //once created we just run the data string against the database and all our new data will be saved/
-  db.collection('users').save(datatostore, function(err, result) {
+  db.collection('people').save(datatostore, function(err, result) {
     if (err) throw err;
     console.log('saved to database')
     //when complete redirect to the index
